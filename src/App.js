e44890792c0617b598c02b608ee766ab9ef2f181
ioncerezo <ioncerezovalero@gmail.com>
2023-01-18 13:36:35 +0000

    Added alphabetical sort

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import React, { useState, useEffect } from 'react';
 import PokeCard from './elements/PokeCard';
 
 function App() {
-  
     const [pokemons, setPokemons] = useState([]);
+    const [pokemonsALL, setPokemonsALL] = useState([]);
+
+    function seeAll(){
+      setPokemons(pokemonsALL)
+    }
+    function sort(letter){
+      const sortedArray = pokemonsALL.filter(pokemon => pokemon.name[0] === letter)
+      setPokemons(sortedArray)
+      
+    }
+    
     useEffect(() => {
-       fetch('https://pokeapi.co/api/v2/pokemon/?limit=10')
+       fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
           .then((response) => response.json())
           .then((allPokemon) => {
             let pokemonlist = []
@@ -15,18 +25,50 @@ function App() {
               
             });
             setPokemons(pokemonlist)
+            setPokemonsALL(pokemonlist)
+            
           })
           .catch((err) => {
              console.log(err.message);
           });
           
     }, []);
-    
+    const btnClass = "inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white"
   return (
-    <div className=' bg-gray-100 h-screen'>
+    <div className=' bg-gray-100 min-h-screen'>
       <h1 className='text-3xl' >Hola</h1>
-      <div className='mx-auto p-2 container flex flex-wrap gap-3'>
-      {pokemons.map(pokemon => <PokeCard url={pokemon.url}/>)}
+      <div className='flex justify-center rounded-md shadow-sm" role="group'>
+        <button className='inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-l-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white' onClick={()=> seeAll()}>ALL</button>
+        <button className={btnClass} onClick={()=> sort('a')}>A</button>
+        <button className={btnClass} onClick={()=> sort('b')}>B</button>
+        <button className={btnClass} onClick={()=> sort('c')}>C</button>
+        <button className={btnClass} onClick={()=> sort('d')}>D</button>
+        <button className={btnClass} onClick={()=> sort('e')}>E</button>
+        <button className={btnClass} onClick={()=> sort('f')}>F</button>
+        <button className={btnClass} onClick={()=> sort('g')}>G</button>
+        <button className={btnClass} onClick={()=> sort('h')}>H</button>
+        <button className={btnClass} onClick={()=> sort('i')}>I</button>
+        <button className={btnClass} onClick={()=> sort('j')}>J</button>
+        <button className={btnClass} onClick={()=> sort('k')}>K</button>
+        <button className={btnClass} onClick={()=> sort('l')}>L</button>
+        <button className={btnClass} onClick={()=> sort('m')}>M</button>
+        <button className={btnClass} onClick={()=> sort('n')}>N</button>
+        <button className={btnClass} onClick={()=> sort('o')}>O</button>
+        <button className={btnClass} onClick={()=> sort('p')}>P</button>
+        <button className={btnClass} onClick={()=> sort('q')}>Q</button>
+        <button className={btnClass} onClick={()=> sort('r')}>R</button>
+        <button className={btnClass} onClick={()=> sort('s')}>S</button>
+        <button className={btnClass} onClick={()=> sort('t')}>T</button>
+        <button className={btnClass} onClick={()=> sort('u')}>U</button>
+        <button className={btnClass} onClick={()=> sort('v')}>V</button>
+        <button className={btnClass} onClick={()=> sort('w')}>W</button>
+        <button className={btnClass} onClick={()=> sort('x')}>X</button>
+        <button className={btnClass} onClick={()=> sort('y')}>Y</button>
+        <button className='inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-r border-gray-900 rounded-r-md hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white' onClick={()=> sort('z')}>Z</button>
+      </div>
+      
+      <div className='mx-auto mt-6 p-2 container  justify-center flex flex-wrap gap-5'>
+      {pokemons.map(pokemon => <PokeCard  name={pokemon.name} key={pokemon.url} url={pokemon.url}/>)}
       
       </div>
     </div>
--- src/elements/PokeCard.js
+++ src/elements/PokeCard.js
@@ -1,52 +1,96 @@
 import React, { useState, useEffect } from "react";
 import TypeBadge from "./TypeBadge";
 
-function PokeCard({ url }) {
+function PokeCard({ url, name }) {
   const [pokemonName, setPokemonName] = useState();
   const [pokemonID, setPokemonID] = useState();
   const [pokemonIMG, setPokemonIMG] = useState();
+  const [pokemonWeight, setPokemonWeight] = useState();
+  const [pokemonBackIMG, setPokemonBackIMG] = useState();
   const [pokemonTypes, setPokemonTypes] = useState([]);
-  const [description, setDescription] = useState([]);
+  const [pokemonDescription, setPokemonDescription] = useState();
+  const [cardOpen, setCardOpen] = useState(false);
+  const url2 = "https://pokeapi.co/api/v2/pokemon-species/" + name;
 
-  
+  function sentenceCase(input, lowercaseBefore) {
+    input = input === undefined || input === null ? "" : input;
+    if (lowercaseBefore) {
+      input = input.toLowerCase();
+    }
+    return input
+      .toString()
+      .replace(/(^|\. *)([a-z])/g, function (match, separator, char) {
+        return separator + char.toUpperCase();
+      });
+  }
+
+  useEffect(() => {
+    fetch(url2)
+      .then((response) => response.json())
+      .then((e) => {
+        const data = e.flavor_text_entries;
+        let result = data
+          .filter((entry) => entry.language.name === "en")[0]
+          .flavor_text.replace("", " ");
+        result = sentenceCase(result, " ");
+        setPokemonDescription(result);
+      });
+  }, [url2]);
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((e) => {
         setPokemonName(e.name[0].toUpperCase() + e.name.slice(1));
         setPokemonID(e.id);
+        setPokemonWeight(e.weight)
+        setPokemonBackIMG(e.sprites.back_default);
         setPokemonIMG(e.sprites.front_default);
         const typeList = [];
         for (let i = 0; i < e.types.length; i++) {
-            typeList.push(e.types[i].type.name)
+          typeList.push(e.types[i].type.name);
         }
-        setPokemonTypes(typeList)
-  
-      })
-      .catch((err) => {
-        console.log(err.message);
+        setPokemonTypes(typeList);
       });
-  });
+  }, [url]);
 
   return (
-    <div className="bg-white shadow-md border border-gray-200 rounded-lg w-40">
-      <div className="w-full h-32 bg-gray-200 flex items-center justify-center hover:opacity-80 transition duration-300 ease-in-out">
+    <div
+      onClick={() => {
+        setCardOpen(!cardOpen)
+        setTimeout(()=>setCardOpen(false),4000)}}
+      className="active:ring-2 cursor-pointer transition duration-300 ease-in-out  bg-white shadow-md border h-64 border-gray-200 rounded-lg w-40"
+    >
+      <div className="w-full h-32 bg-gray-200 flex items-center justify-center hover:opacity-80 transition duration-500 ease-in-out">
         <img
-          className="rounded-t-lg "
-          src={pokemonIMG}
+          className=" select-none overflow-hidden whitespace-nowrap indent-[100%]"
+          src={cardOpen ? pokemonBackIMG : pokemonIMG}
           alt={`sprite of ${pokemonName}`}
+          
         ></img>
       </div>
-      <div className="p-5">
+      {
+        cardOpen ? 
+        <div className="h-32 flex p-2 justify-center items-center">
+        <p className="text-sm italic text-gray-900 " >{pokemonDescription}</p>
+        </div>
+        :
+        <div className="p-5">
+        <div className="flex justify-between mb-2">
         <h6 className="text-gray-700 font-bold text-xs">N.º{pokemonID}</h6>
-        <h5 className="text-gray-900 font-bold text-lg tracking-tight mb-2">
+        <h6 className="text-gray-700 italic text-xs">{pokemonWeight/10}kg</h6>
+        </div>
+        <h5 className="text-gray-900 font-bold text-lg text-center tracking-tight mb-2">
           {pokemonName}
         </h5>
-        <div className="flex gap-1">
-          {pokemonTypes.map(type => <TypeBadge key={`${pokemonName}-${type}`} type={type}/>)}
+        <div className="flex gap-1 justify-center select-none">
+          {pokemonTypes.map((type) => (
+            <TypeBadge key={`${pokemonName}-${type}`} type={type} />
+          ))}
         </div>
-        
       </div>
+      }
+      
+      
     </div>
   );
 }
